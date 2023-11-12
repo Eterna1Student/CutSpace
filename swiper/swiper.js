76ca199da1612c2788aeeb653a56c39c2ac591cc
Eterna1Student <oleg-kozlov91@rambler.ru>
2023-11-13 02:14:50 +0400

    подчистил, добавил swiperJS

---
+++ swiper/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 4000,
+    autoplay: {
+        delay: 0,
+      },
+    slidesPerView: 4,
+    spaceBetween: 10,
+  });
